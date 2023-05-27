fc8dc6dad79bee661bea7ac32205753e97a83de1
Ivan Shestakov <vanyww@mail.ru>
2023-05-27 21:24:53 +0300

    Fix missing config macro rename in crc.c

--- Src/Rtu/Crc/crc.c
+++ Src/Rtu/Crc/crc.c
@@ -1,5 +1,5 @@
 #include "crc.h"
 
-#ifndef MODBUS_RTU_SDEVICE_USE_EXTERN_CRC
+#if defined MODBUS_RTU_SDEVICE_USE_INTERNAL_CRC
 SDEVICE_HANDLE(TableCrc16) *ModbusRtuSDeviceInternalCrc16Handle = NULL;
 #endif
