266cd57080326dd9f88b371874c47eb4725146e3
Ivan Shestakov <vanyww@mail.ru>
2024-01-11 22:59:39 +0300

    Fix Modbus RTU's TableCrc SDevice preprocessor checks

--- Inc/ModbusSDevice/Rtu/dependencies.h
+++ Inc/ModbusSDevice/Rtu/dependencies.h
@@ -6,8 +6,8 @@
 
 #if MODBUS_RTU_SDEVICE_USE_INTERNAL_CRC
    #include "TableCrcSDevice/Crc16/public.h"
-#endif
 
-#if (TABLE_CRC16_SDEVICE_VERSION_MAJOR != 4) || (TABLE_CRC16_SDEVICE_VERSION_MINOR < 0)
-   #error TableCrc16 SDevice version is incorrect.
+   #if (TABLE_CRC16_SDEVICE_VERSION_MAJOR != 4) || (TABLE_CRC16_SDEVICE_VERSION_MINOR < 0)
+      #error TableCrc16 SDevice version is incorrect.
+   #endif
 #endif
