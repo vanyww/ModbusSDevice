7abe544ce607be0ff054ae417fbb09b82447d53b
Avvesey Erockhov <kolyann088@gmail.com>
2025-05-13 17:55:40 +0300

    Update 'TableCrcSDevice' version in dependencies

--- Inc/ModbusSDevice/Rtu/dependencies.h
+++ Inc/ModbusSDevice/Rtu/dependencies.h
@@ -5,7 +5,7 @@
 #if MODBUS_RTU_SDEVICE_USE_INTERNAL_CRC
    #include "TableCrcSDevice/Crc16/public.h"
 
-   #if (TABLE_CRC16_SDEVICE_VERSION_MAJOR != 4) || (TABLE_CRC16_SDEVICE_VERSION_MINOR < 0)
+   #if (TABLE_CRC16_SDEVICE_VERSION_MAJOR != 5) || (TABLE_CRC16_SDEVICE_VERSION_MINOR < 0)
       #error TableCrc16 SDevice version is incorrect.
    #endif
 #endif
